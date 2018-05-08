dbdae940e8b8d4a513a78391db4876cd42123725
Christopher Henry Simmons <christopher.simmons@protonmail.ch>
2018-05-08 13:09:30 +1200

    Fix?

--- database.php
+++ database.php
@@ -1,37 +1,8 @@
 <?php
-class Database
-{
-    private static $dbName = '8BITGAME' ;
-    private static $dbHost = 'localhost' ;
-    private static $dbUsername = 'root';
-    private static $dbUserPassword = 'root';
-     
-    private static $cont  = null;
-     
-    public function __construct() {
-        die('Init function is not allowed');
-    }
-     
-    public static function connect()
-    {
-       // One connection through whole application
-       if ( null == self::$cont )
-       {     
-        try
-        {
-          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
-        }
-        catch(PDOException $e)
-        {
-          die($e->getMessage()); 
-        }
-       }
-       return self::$cont;
-    }
-     
-    public static function disconnect()
-    {
-        self::$cont = null;
-    }
-}
-?>
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbName = "8BITDB";
+
+$conn = mysqli_connect($dbServername,$dbUsername, $dbPassword, $dbName);
